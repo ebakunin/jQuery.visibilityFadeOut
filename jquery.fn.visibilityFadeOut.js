2d0ce34f70a0184fe063df8231225a89825a4684
Eric Christenson <ebakunin@gmail.com>
2015-02-22 20:10:32 -0700

    Initial file and example.

---
+++ jquery.fn.visibilityFadeOut.js
@@ -0,0 +1,21 @@
+﻿/**
+ * A version of jQuery.fadeOut() that uses 'visibility' CSS styling
+ *
+ * @author   Eric Christenson
+ * @date     February 22, 2015
+ * @version  1.0
+ * @license  MIT License (http://opensource.org/licenses/MIT)
+ *
+ * @param  fadeSpeed  int (time in milliseconds. Optional, defaults to 1 second)
+ */
+(function ($) {
+    $.fn.visibilityFadeOut = function (fadeSpeed) {
+        fadeSpeed = fadeSpeed || 1000;
+
+        $(this).each(function (index) {
+            $(this).delay(index * 100).fadeTo(parseInt(fadeSpeed), 0, function () {
+                $(this).css({ visibility: 'hidden', opacity: 1 });
+            });
+        });
+    }
+})(jQuery);
